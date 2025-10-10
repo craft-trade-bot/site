180317c1a83979ad7dd9a533b873b6312d27cb5e
cooler <you@example.com>
2025-10-10 21:40:17 +0800

    Revert "test big clear"
    This reverts commit ebe2e2093072d537d10a9e93af2bf15389565742.

---
+++ assets/vendor/startbootstrap-clean-blog/js/scripts.js
@@ -0,0 +1,37 @@
+/*!
+* Start Bootstrap - Clean Blog v5.1.0 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
+*/
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Show the navbar when the page is scrolled up
+    var MQL = 992;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('#mainNav').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function () {
+                var currentTop = $(window).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+                        $('#mainNav').addClass('is-visible');
+                    } else {
+                        $('#mainNav').removeClass('is-visible is-fixed');
+                    }
+                } else if (currentTop > this.previousTop) {
+                    //if scrolling down...
+                    $('#mainNav').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+                }
+                this.previousTop = currentTop;
+            });
+    }
+
+})(jQuery); // End of use strict
