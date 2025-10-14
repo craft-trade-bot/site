fb4b67d7a7658a5ff1212a5ab202295f761b04df
cooler <you@example.com>
2025-10-14 13:00:20 +0800

    init_new_antimatter

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
-FROM ruby:3.1
-
-RUN mkdir /app
+FROM node:lts AS build
 WORKDIR /app
 
-COPY Gemfile .
-COPY Gemfile.lock .
-
-RUN bundle install
+COPY package.json .
+COPY package-lock.json .
+RUN npm install
 
 COPY . .
+RUN npm run build
 
-CMD ["bundle", "exec", "jekyll", "serve", "--host", "0.0.0.0", "--trace"]
+FROM nginx:alpine AS runtime
+COPY ./nginx/nginx.conf /etc/nginx/nginx.conf
+COPY --from=build /app/dist /usr/share/nginx/html
+EXPOSE 8080
---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE       = 'Craft Trade';
+export const SITE_DESCRIPTION = 'Современное приложение для создания торговых ботов на крипторынке';
+
+export const TELEGRAM_BOT     = "https://t.me/craft_trade_bot?start=official"
+export const TELEGRAM_CHANNEL = "https://t.me/+NL0KuVkfenE2Yzcy"
+export const LINK_DOC         = "/blog"
