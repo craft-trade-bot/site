ea4d15e3ed6c11346cc516ce25d3a7c6f28585ab
cooler <you@example.com>
2025-10-20 10:52:47 +0800

    descr

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE       = 'Craft Trade';
-export const SITE_DESCRIPTION = 'Современное приложение для создания торговых ботов на крипторынке';
+export const SITE_DESCRIPTION = 'CraftTradeBot — современное приложение для создания и управления торговыми ботами на крипторынке. Интуитивный интерфейс и гибкие настройки позволят автоматизировать трейдинг и повысить эффективность ваших торговых стратегий';
 
 export const TELEGRAM_BOT     = "https://t.me/craft_trade_bot?start=official"
 export const TELEGRAM_CHANNEL = "https://t.me/+NL0KuVkfenE2Yzcy"
