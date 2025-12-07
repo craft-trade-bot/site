ef986dbf5b45d54deaeef846ab81c9aafc4e173c
vir <you@example.com>
2025-12-07 21:09:17 +0800

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://crafttradebot.ru',
+  site: 'https://crafttrade-bot.ru',
   integrations: [mdx(), sitemap()],
 
   vite: {
